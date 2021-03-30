d718dd660730e1a92af8a05b326ca3e35434a089
Ilmar <ilmar.ilves44@gmail.com>
2021-03-30 21:01:16 +0300

    Added components, implemented arrow first draft

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { MapView } from './views/MapView/MapView';
+
+function App() {
+  return (
+    <>
+      <MapView />
+    </>
+  );
+}
+
+export default App;
